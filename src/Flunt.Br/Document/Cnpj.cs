872f5f5750cd70417a640a8101109a09853e514c
Alan Lira <alan_lirasp@hotmail.com>
2019-05-22 21:33:28 -0300

    fix cnpj validation when there are letters in the input upgrade dependencies

--- src/Flunt.Br/Document/Cnpj.cs
+++ src/Flunt.Br/Document/Cnpj.cs
@@ -1,4 +1,5 @@
 using Flunt.Br.Document.interfaces;
+using System;
 
 namespace Flunt.Br.Document
 {
@@ -6,36 +7,30 @@ namespace Flunt.Br.Document
     {
         public bool Validate(string value)
         {
-             int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            var multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            var multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
             int soma;
             int resto;
             string digito;
             string tempCnpj;
             value = value.Trim();
             value = value.Replace(".", "").Replace("-", "").Replace("/", "");
-            if (value.Length != 14)
+            if (value.Length != 14 || !long.TryParse(value, out var parsed))
                 return false;
             tempCnpj = value.Substring(0, 12);
             soma = 0;
             for (int i = 0; i < 12; i++)
                 soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
             resto = (soma % 11);
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
+            resto = resto < 2 ? 0 : 11 - resto;
             digito = resto.ToString();
             tempCnpj = tempCnpj + digito;
             soma = 0;
             for (int i = 0; i < 13; i++)
                 soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
             resto = (soma % 11);
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
-            digito = digito + resto.ToString();
+            resto = resto < 2 ? 0 : 11 - resto;
+            digito = digito + resto;
             return value.EndsWith(digito);
         }
     }
