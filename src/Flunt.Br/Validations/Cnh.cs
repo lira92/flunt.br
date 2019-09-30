b4f2d82e93c40f03fe54b384ec70922c2b56c3ff
giribroba <giribroba@gmail.com>
2019-09-30 19:01:13 -0300

    validação cnh

---
+++ src/Flunt.Br/Validations/Cnh.cs
@@ -0,0 +1,43 @@
+﻿using Flunt.Br.Document.Interfaces;
+
+namespace Flunt.Br.Validations
+{
+    internal class Cnh : IValidate
+    {
+        public bool Validate(string value)
+        {
+            int[] multiplicador = { 9, 10 };
+            string dvRecebido, cnhSemDv, dvCalculado = "";
+            int soma = 0;
+
+            value = value.Trim().Replace(",", "").Replace(".", "").Replace("-", "").Replace("_", "");
+
+            if (value.Length == 11 && value != "00000000000")
+            {
+                dvRecebido = value.Substring(9, 2);
+                cnhSemDv = value.Substring(0, 9);
+
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        soma += int.Parse(cnhSemDv[j].ToString()) * multiplicador[i];
+                        multiplicador[i]--;
+                    }
+                    var temp = soma % 11;
+                    if (temp >= 10)
+                        dvCalculado += 1;
+                    else
+                    {
+                        dvCalculado += temp;
+                    }
+                    soma = 0;
+                }
+
+                return dvRecebido == dvCalculado;
+            }
+            else
+                return false;
+        }
+    }
+}
